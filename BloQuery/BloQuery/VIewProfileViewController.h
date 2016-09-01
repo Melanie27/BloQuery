e3a9ca9c4b199109c8bf8c4fb7137a666364e119
Melanie27 <melaniemcganney@gmail.com>
2016-09-01 16:47:49 -0700

    new view controller

---
+++ BloQuery/BloQuery/VIewProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  VIewProfileViewController.h
+//  BloQuery
+//
+//  Created by MELANIE MCGANNEY on 9/1/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "UserProfileViewController.h"
+
+@interface VIewProfileViewController : UserProfileViewController
+
+@end
---
+++ BloQuery/BloQuery/VIewProfileViewController.m
@@ -0,0 +1,37 @@
+//
+//  VIewProfileViewController.m
+//  BloQuery
+//
+//  Created by MELANIE MCGANNEY on 9/1/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "VIewProfileViewController.h"
+
+@interface VIewProfileViewController ()
+
+@end
+
+@implementation VIewProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
