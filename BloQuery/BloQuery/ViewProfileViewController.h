a4566eb83135a3f998c28af1d1cd94ac438195ed
Melanie27 <melaniemcganney@gmail.com>
2016-09-01 11:20:56 -0700

    delete unused add subclass VC for profile view

---
+++ BloQuery/BloQuery/ViewProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewProfileViewController.h
+//  BloQuery
+//
+//  Created by MELANIE MCGANNEY on 9/1/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "UserProfileViewController.h"
+
+@interface ViewProfileViewController : UserProfileViewController
+
+@end
